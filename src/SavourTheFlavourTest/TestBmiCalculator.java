b14a3af5c5b777a8ae8e6db5797066d22af7d00a
Mriitika150 <mrittikajahan@iut-dhaka.edu>
2025-04-06 12:07:33 +0600

    Test case for bmi calculator

---
+++ src/SavourTheFlavourTest/TestBmiCalculator.java
@@ -0,0 +1,22 @@
+package SavourTheFlavourTest;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Scanner;
+import SavourTheFlavourMain.BmiCalculator;
+
+public class TestBmiCalculator {
+    @Test
+    public void testBMICalculationSimple() {
+        double bmi = BmiCalculator.calculateBMI(70, 170);
+        assertEquals(24.22, bmi, 0.01);
+    }
+    @Test
+    public void testBMICalculation_Imperial() {
+        
+        double weightKg = 154 * 0.453592;
+        double heightCm = ((5 * 12) + 7) * 2.54;
+
+        double bmi = BmiCalculator.calculateBMI(weightKg, heightCm);
+        assertEquals(24.1, bmi, 0.05);
+    }
+}
